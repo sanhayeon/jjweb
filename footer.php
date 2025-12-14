1f80c8b4e7910c54f0389c70cd488e8788afed15
sanhayeon <117253016+sanhayeon@users.noreply.github.com>
2025-12-14 20:38:02 +0900

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+            </div>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="/static/js/scripts.js"></script>
+		
+		<script src="https://cdn.jsdelivr.net/npm/prismjs@1/prism.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/prismjs@1/components/prism-php.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/prismjs@1/components/prism-sql.min.js"></script>
+
+    </body>
+</html>
+
